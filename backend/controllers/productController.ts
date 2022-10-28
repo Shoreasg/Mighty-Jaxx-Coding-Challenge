9924ae43d95f6d9660b7c39cf5e2b3796d2a4937
Wei Lun <garyganweilun@gmail.com>
2022-10-28 17:21:04 +0800

    Done edit item

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -13,16 +13,15 @@ const authMiddleWare = (req: Request, res: Response, next: NextFunction) => {
         jwt.verify(token, process.env.JWTSECRET, (err: any, user: any) => { //verify jwt
             if (err) {
                 //if error, tell user that their token is invalid
-                return res.status(403).json({Error:"Token is not valid!"});
+                return res.status(403).json({ Error: "Token is not valid!" });
             }
             //if token is valid, check for expiry time next
-            if(user.exp * 1000 < new Date().getTime())
-            {
+            if (user.exp * 1000 < new Date().getTime()) {
                 // if the current date time is greater than the expiry time, tell frontend that their token is invalid
-                return res.status(401).json({Error: "Token is invalid"})
+                return res.status(401).json({ Error: "Token is invalid" })
             }
             // if token is really valid at this stage, get the payload user email,then add the user email in the req body.
-            req.body.authUser = user.email
+            req.user = user.email // req.user, define the type on custom.d.ts
             next();
         })
     } else {
@@ -31,13 +30,12 @@ const authMiddleWare = (req: Request, res: Response, next: NextFunction) => {
 }
 
 
-
+// add product
 router.post('/addProduct', authMiddleWare, async (req: Request, res: Response) => {
 
     const { SKU, title, imageURL } = req.body;
-
     // double check if the user exist in our DB to make sure he is authenticated
-    const checkUser = await User.findOne({email:req.body.authUser})
+    const checkUser = await User.findOne({ email: req.user })
 
     if (checkUser) {
         try {
@@ -62,14 +60,69 @@ router.post('/addProduct', authMiddleWare, async (req: Request, res: Response) =
         } catch (err) {
             return res.status(500).json(err);
         }
-      
+
 
     }
-    else{
+    else {
         // tell frontend that this guy don't exist in our DB, don't allow him to do request
-     res.status(403).json("You are not allowed to do this request!");       
+        res.status(403).json("You are not allowed to do this request!");
     }
 
 })
+
+//edit
+router.put('/editProduct/:oldSKU', authMiddleWare, async (req: Request, res: Response) => {
+    const { newSKU, newTitle, newImageURL } = req.body
+    const { oldSKU } = req.params;
+    // double check if the user exist in our DB to make sure he is authenticated
+    const checkUser = await User.findOne({ email: req.user })
+    if (checkUser) {
+        try {
+            //search for the selected product to be updated in our db
+            const selectedProduct = await Product.findOne({SKU: oldSKU});
+            //if found, most likely will always be found unless frontend send wrong data.
+            if(selectedProduct)
+            {
+                   //check if the req body contains empty string, else give error
+                   if (typeof newSKU === "string" && newSKU.trim().length === 0 ||
+                   typeof newTitle === "string" && newSKU.trim().length === 0 ||
+                   typeof newImageURL === "string" && newSKU.trim().length === 0) {
+                   return res.status(400).json({ Error: "Invalid Request, check that it does not contain any empty strings" });
+               }
+               
+                    //check if the new sku/title and imageurl exist in DB, if yes, give error
+                    const checkExistingSKU = await Product.findOne({ SKU: newSKU });
+                    const checkExistingTitle = await Product.findOne({ title: newTitle });
+                    const checkExistingImage = await Product.findOne({imageURL: newImageURL});
+
+                    if(checkExistingImage||checkExistingSKU|| checkExistingTitle)
+                    {
+                        return res.status(400).json({Error:"Invalid Request, You have entered an existing image/sku/title"})
+                    }
+                    
+                    //after all checks, update the product and tell user update successfully
+                    await Product.findByIdAndUpdate(selectedProduct._id,{SKU: newSKU, title: newTitle, imageURL: newImageURL});
+
+                    return res.status(200).json({
+                        Message: "Updated Sucessfully",
+                    })
+            }
+            else{
+                //else if selected item not found, tell user that the item he selected doesn't exist in the database
+                return res.status(400).json({Error:"The item that you selected doesn't exist in the database!"})
+            }            
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }
+    else {
+        // tell frontend that this guy don't exist in our DB, don't allow him to do request
+        res.status(403).json("You are not allowed to do this request!");
+    }
+})
+
+
+// delete an item
+
 
 module.exports = router
