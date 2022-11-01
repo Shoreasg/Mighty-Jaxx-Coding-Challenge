7543fd2ede9c00d2f925047df28da900874b0b3c
Wei Lun <garyganweilun@gmail.com>
2022-11-01 16:55:21 +0800

    Edited some backend logic"

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -48,16 +48,15 @@ router.post("/login", async (req: Request, res: Response,) => {
         // else compare user password with our db hashed password
         const checkPassword = await bcrypt.compare(password, findUser.password);
 
-        if(!checkPassword)
-        {   
+        if (!checkPassword) {
             //if compared and the result is false, we tell user he entered the wrong credentials
-            return res.status(401).json({Error: "Invalid credentials"});
+            return res.status(401).json({ Error: "Invalid credentials" });
         }
 
         //else we create a token and respond the token to frontend
-        const jwtToken = await jwt.sign({email: findUser.email},process.env.JWTSECRET,{expiresIn: '1h'});
+        const jwtToken = await jwt.sign({ email: findUser.email }, process.env.JWTSECRET, { expiresIn: '1h' });
 
-        return res.status(200).json({accessToken: jwtToken, token_type: 'Bearer', expires_in: "1h"})
+        return res.status(200).json({ accessToken: jwtToken, token_type: 'Bearer', expires_in: "1h" })
 
 
 
@@ -66,6 +65,22 @@ router.post("/login", async (req: Request, res: Response,) => {
     }
 
 
+})
+
+router.post("/checkUser", async (req: Request, res: Response,) => { //check if token has expired, if not send the user email to frontend
+    const { jwtToken } = req.body;
+
+    try {
+        jwt.verify(jwtToken, process.env.JWTSECRET, (err: any, user: any) => {
+            if (err) {
+                //if error, tell user that their token is invalid
+                return res.status(403).json({ Error: "Invalid User / Session Expired, Please relogin" });
+            }
+            res.status(200).json(user.email)
+        })
+    } catch (err) {
+        return res.status(500).json(err);
+    }
 })
 
 module.exports = router
